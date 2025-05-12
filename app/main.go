f32ad501e9d11dd8848292ecb0a98e6e064a70b4
Lucas Hope <lucasfhope@icloud.com>
2025-05-12 01:26:58 -0400

    .

---
+++ app/main.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+const (
+	defaultTarget   = "/"
+	echoTarget      = "/echo/"
+	userAgentTarget = "/user-agent"
+	filesTarget     = "/files/"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+	fmt.Println("Listening on port 4221...")
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Print("Error accepting connection:\n", err.Error(), "\n")
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+///////////////////////////////////
+// Connection Request Handling  //
+//////////////////////////////////
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	request, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		return
+	}
+
+	requestArray := strings.Split(request, " ")
+	if len(requestArray) != 3 {
+		fmt.Println("Invalid request format")
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
+	}
+	method := requestArray[0]
+	target := requestArray[1]
+	fmt.Println(fmt.Sprintf("Received %s request for target %s", method, target))
+
+	headers := make(map[string]string)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading headers:", err.Error())
+			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			return
+		}
+		line = strings.TrimSpace(line)
+
+		if line == "" {
+			break
+		}
+
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			headers[key] = value
+		}
+	}
+
+	body := []byte{}
+	contentLengthStr, hasBody := headers["Content-Length"]
+	if hasBody {
+		var contentLength int
+		_, err := fmt.Sscanf(contentLengthStr, "%d", &contentLength)
+		if err != nil {
+			fmt.Println("Invalid Content-Length:", contentLengthStr)
+			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			return
+		}
+
+		body = make([]byte, contentLength)
+		_, err = reader.Read(body)
+		if err != nil {
+			fmt.Println("Error reading body:", err)
+			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			return
+		}
+	}
+
+	if method == "GET" {
+		handleGetRequest(conn, target, headers, body)
+	} else if method == "POST" {
+		handlePostRequest(conn, target, headers, body)
+	} else {
+		conn.Write([]byte("HTTP/1.1 405 Method Not Allowed\r\nAllow: GET, POST\r\n\r\n"))
+	}
+}
+
+func handleGetRequest(conn net.Conn, target string, headers map[string]string, body []byte) {
+	if target == defaultTarget {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.HasPrefix(target, echoTarget) {
+		getEcho(conn, target)
+	} else if target == userAgentTarget {
+		getUserAgent(conn, headers)
+	} else if strings.HasPrefix(target, filesTarget) {
+		getFile(conn, target)
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+}
+
+func handlePostRequest(conn net.Conn, target string, headers map[string]string, body []byte) {
+	if strings.HasPrefix(target, filesTarget) {
+		postFile(conn, target, body)
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+}
+
+////////////////////////////
+// GET Request Functions  //
+////////////////////////////
+
+func getEcho(conn net.Conn, target string) {
+	echoMessage := strings.TrimSpace(strings.TrimPrefix(target, echoTarget)) + "\n"
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoMessage), echoMessage)
+	conn.Write([]byte(response))
+}
+
+func getUserAgent(conn net.Conn, headers map[string]string) {
+	userAgent, exists := headers["User-Agent"]
+	if !exists {
+		fmt.Println("User-Agent header not found")
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
+	}
+	userAgent = strings.TrimSpace(userAgent) + "\n"
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+	conn.Write([]byte(response))
+}
+
+func getFile(conn net.Conn, target string) {
+	filePath := "." + target
+	fmt.Println("File path:", filePath)
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Print("Error reading file:\n", err.Error(), "\n")
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
+	}
+	data = append(data, '\n')
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)
+	conn.Write([]byte(response))
+}
+
+////////////////////////////
+// POST Request Handlers  //
+////////////////////////////
+
+func postFile(conn net.Conn, target string, body []byte) {
+	fileName := strings.TrimSpace(strings.TrimPrefix(target, filesTarget))
+
+	// Create the files directory if it doesn't exist
+	dirPath := "./files"
+	err := os.MkdirAll(dirPath, 0700)
+	if err != nil {
+		fmt.Print("Error creating directory\n", err.Error(), "\n")
+		conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+		return
+	}
+
+	// Create the file
+	filePath := dirPath + "/" + fileName
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Print("Error creating file:\n", err.Error(), "\n")
+		conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+		return
+	}
+	defer file.Close()
+
+	fmt.Println(body)
+
+	// Write the body to the file
+	_, err = file.Write(body)
+	if err != nil {
+		fmt.Print("Error writing to file\n", err.Error(), "\n")
+		conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+		return
+	}
+
+	fmt.Println("File created successfully:", filePath)
+	conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucasfhope/golang-http-server
+
+go 1.24.3
